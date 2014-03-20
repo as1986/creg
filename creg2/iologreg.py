7854a85736e190cebd33a17c19fefdeab84f93a2
Ki-Cheng Lim <chu.cheng.lin@gmail.com>
2014-03-19 22:07:19 -0400

    l2

--- creg2/iologreg.py
+++ creg2/iologreg.py
@@ -106,7 +106,8 @@ class IOLogisticRegression:
 
             if using_l2:
                 intermed = np.divide(U, np.sqrt(H))
-                self.W = intermed * eta * (i + 1) / self.l1
+                self.W = intermed * eta * (i + 1)
+                print 'l1: {}'.format(self.l1)
             else:
                 threshold = np.maximum(np.subtract(np.divide(np.absolute(U), i + 1), ld),
                                        np.zeros(shape=(infeats, outfeats)))
