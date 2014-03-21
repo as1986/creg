59f49002bfef66086c54f818b3c0e2deb263cad5
Ki-Cheng Lim <chu.cheng.lin@gmail.com>
2014-03-21 02:44:04 -0400

    bias

--- creg2/creg2.py
+++ creg2/creg2.py
@@ -243,6 +243,7 @@ else:
 if args.tx is not None and args.ty is not None:
     import numpy
 
+    print bias_vec
     model = fit_model(labels, label_features, out_dim, in_dim, X, Y, N, 'model_output', load=args.loadmodel,
                       iterations=args.iterations, warm_start=args.warm, l1=numpy.power(10, args.l1),bias=bias_vec)
 
