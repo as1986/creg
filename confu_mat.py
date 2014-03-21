e8c29d57ca8284b15f2479e2e2f7d08b48fe780f
Ki-Cheng Lim <chu.cheng.lin@gmail.com>
2014-03-21 19:35:40 -0400

    csv cm

--- confu_mat.py
+++ confu_mat.py
@@ -4,18 +4,27 @@ __author__ = 'as1986'
 
 
 def get_confu_mat(rows):
+    import sys
+
     predictions = ([x[0] for x in rows[1:]])
     answers = ([x[1] for x in rows[1:]])
     label_set = set.union(set(predictions), set(answers))
     indexed_labels = {k: v for v, k in enumerate(label_set)}
+    inversed = {v: k for v, k in enumerate(label_set)}
     one_row = [0] * len(label_set)
     table = [list(one_row) for k in range(len(label_set))]
     for (pred, ans, id) in rows[1:]:
         table[indexed_labels[pred]][indexed_labels[ans]] += 1
 
-    print ' \t' + '\t'.join([str(x) for x in range(len(label_set))])
-    for idx, table_row in enumerate(table):
-        print '{}\t'.format(idx) + '\t'.join([str(x) for x in table_row])
+    with open(sys.argv[1] + '.cm.csv', 'w') as cf:
+        import csv
+
+        writer = csv.writer(cf)
+        header_row = ['X'] + [inversed[x] for x in range(len(label_set))]
+        writer.write(header_row)
+        for idx, table_row in enumerate(table):
+            row = ['{}'.format(inversed[idx])] + table_row
+            writer.write(row)
 
     print indexed_labels
 
